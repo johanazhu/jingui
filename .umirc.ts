9e6b3c6d9f22a8ac18ccef77ae6a9352825983b2
johanazhu <t511069160@qq.com>
2021-07-07 14:08:50 +0800

    '修改一些基本配置'

--- .umirc.ts
+++ .umirc.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
     title: 'jing-ui',
-    favicon: 'https://i.loli.net/2021/07/07/c1Ivk7yYt3FpBKs.png',
-    logo: 'https://i.loli.net/2021/07/07/c1Ivk7yYt3FpBKs.png',
+    favicon: 'https://i.loli.net/2021/07/07/jV4wc98UW5ELlCS.png',
+    logo: 'https://i.loli.net/2021/07/07/jV4wc98UW5ELlCS.png',
     outputPath: 'docs-dist',
     mode: 'site',
-    navs: [null, { title: 'GitHub', path: 'https://github.com/umijs/dumi' }],
+    navs: [null, { title: 'GitHub', path: 'https://github.com/johanazhu/jingui' }],
     themeConfig: {
         hd: {
             // 根据不同的设备屏幕宽度断点切换高清方案
@@ -16,6 +16,10 @@ export default defineConfig({
             ],
             // 更多 rule 配置访问 https://github.com/umijs/dumi/blob/master/packages/theme-mobile/src/typings/config.d.ts#L7
         },
+    },
+    targets: {
+        android: 4,
+        ios: 8,
     },
     chainWebpack(memo: any, args: any) {
         memo.module.rules.delete('svg');
