ea4746569f0e0962fe7ed58026d99be0c9e80d0f
johanazhu <t511069160@qq.com>
2021-12-07 15:10:24 +0800

    'fix_warningLF'

--- src/components/Sticky/PropType.tsx
+++ src/components/Sticky/PropType.tsx
@@ -1,5 +1,11 @@
 import { ReactNode, CSSProperties } from 'react';
 
+/**
+ * Sticky 粘性布局
+ * @param {className} string                        // 自定义 class 名
+ * @param {style} CSSProperties                     // 样式
+ * @param {children} ReactNode                      // 组件中的内容
+ **/
 export interface StickyProps {
     className?: string;
     style?: CSSProperties;
