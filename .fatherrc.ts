b90a3456fbfc774cdd3b16be11591fad97c94197
johanazhu <t511069160@qq.com>
2023-01-31 13:16:17 +0800

    更新使用father打包，再发一次

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'father';
 
 export default defineConfig({
-    // https://github.com/umijs/father/tree/2.x
-    // entry: 'src/index.ts',
-    // esm: 'rollup',
     esm: {
         output: 'es'
     },
@@ -15,34 +12,4 @@ export default defineConfig({
         output: 'dist',
         extractCSS: true,
     },
-    // umd: {
-    //     name: 'jingui',
-    //     globals: {
-    //         react: 'React',
-    //         'react-dom': 'ReactDOM',
-    //     },
-    // },
-    // umd: {
-    //     file: 'index',
-    //     sourcemap: true,
-    // },
-    // cjs: 'babel',
-    // umd: {
-    //     name: 'jingui',
-    //     globals: {
-    //         react: 'React',
-    //         // antd: 'antd',
-    //     },
-    // },
-    // extraBabelPlugins: [
-    //     ['babel-plugin-import', {
-    //         libraryName: 'antd',
-    //         libraryDirectory: 'es',
-    //         style: true,
-    //     }]
-    // ],
-    // extractCSS: true,
-    // sassInRollupMode: {
-
-    // }
 });
